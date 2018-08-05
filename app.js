0c5fb06042c04d4ab1f203ddb22b518ffc748588
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-08-05 11:12:38 -0400

    starting the vidjot application

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+//Index Route
+
+app.get('/', (req, res) => {
+    res.send('INDEX');
+});
+
+
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+const port = 5000;
+
+app.listen(port,() => {
+    console.log(`Server started on port ${port}`);
+})
